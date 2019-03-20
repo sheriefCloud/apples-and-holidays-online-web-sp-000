02ccb177f6551a0efec3b056b25c7b35c1ab7e5f
Sherief Farid <sherief@farid.cloud>
2019-03-20 01:59:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday,items|
-      puts "  #{holiday.to_s.split("_").collect {|h| h.capitalize}.join (" ")}: #{items}.join(", ")" 
+      puts "  #{holiday.to_s.split("_").collect {|h| h.capitalize}.join (" ")}: #{items.join(", ")}" 
     end
   end
 
